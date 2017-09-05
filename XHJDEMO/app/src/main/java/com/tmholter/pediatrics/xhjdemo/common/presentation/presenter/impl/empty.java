370f218ccd14f54a297b383e1a10a98756417b89
niluogege <835350313@qq.com>
2017-09-05 11:20:22 +0800

    add empty dir

---
+++ XHJDEMO/app/src/main/java/com/tmholter/pediatrics/xhjdemo/common/presentation/presenter/impl/empty.java
@@ -0,0 +1,11 @@
+package com.tmholter.pediatrics.xhjdemo.common.presentation.presenter.impl;
+
+/**
+ * Created by ${LuoChen} on 2017/9/5 11:15.
+ * email:luochen0519@foxmail.com
+ *
+ *  因为github 不让传空的文件夹 所以用来占位子
+ */
+
+public class empty {
+}
