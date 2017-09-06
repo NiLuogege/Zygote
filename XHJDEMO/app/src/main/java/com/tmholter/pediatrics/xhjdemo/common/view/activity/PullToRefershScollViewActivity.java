a4b7ec4c143f928c4d9b8161d4c8133f45feea6c
niluogege <835350313@qq.com>
2017-09-06 15:28:24 +0800

    ScollView的下拉刷新控件 增加了 自动刷新功能

--- XHJDEMO/app/src/main/java/com/tmholter/pediatrics/xhjdemo/common/view/activity/PullToRefershScollViewActivity.java
+++ XHJDEMO/app/src/main/java/com/tmholter/pediatrics/xhjdemo/common/view/activity/PullToRefershScollViewActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.tmholter.pediatrics.xhjdemo.R;
 import com.tmholter.pediatrics.xhjdemo.common.view.view.PullScrollView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by ${LuoChen} on 2017/9/6 14:11.
@@ -32,9 +35,10 @@ public class PullToRefershScollViewActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Log.e("PullToRefershScollViewA", "数据加载成功");
                 psv.stopRefresh();
             }
-        },2000);
+        }, 2000);
 
         psv.setOnRefreshListener(new PullScrollView.onRefreshListener() {
             @Override
@@ -42,11 +46,18 @@ public class PullToRefershScollViewActivity extends AppCompatActivity {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        Log.e("PullToRefershScollViewA", "数据加载成功");
                         psv.stopRefresh();
                     }
-                }, 1500);
+                }, 2000);
             }
         });
+    }
+
+
+    @OnClick(R.id.tv)
+    public void click() {
+        Toast.makeText(this, "lalal", Toast.LENGTH_LONG).show();
     }
 
 
